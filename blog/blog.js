2672f763a4e11f9ae21f1386dd44f8033fc2d3ba
OliverBrotchie <o.brotchie@gmail.com>
2020-05-13 14:13:41 +0100

    blog development

--- blog/blog.js
+++ blog/blog.js
@@ -1,24 +1,86 @@
+var script = document.createElement("script");
+script.src = 'https://unpkg.com/showdown/dist/showdown.min.js';  
+document.body.appendChild(script);
+
+
 const converter = new showdown.Converter();
+
+function Blog(path) {
+    this.path = path;
+    this.articles = [];
+}
+
+Blog.prototype.add = function(article){
+    if(article.html == undefined){
+
+        fetch(this.path + article.path + "/" + article.title + ".md")
+        .then(response => {
+            return response.text();
+        }).then(text =>{
+            article.html = convert(text);
+            this.articles.push(article);
+        })
+
+    } else this.articles.push(article);
+}
+
+Blog.prototype.indexOf = function(title){
+    var found = -1;
+
+    for(var i = 0;i < this.articles.length;i++){
+        if(this.articles[i].title == title) {
+            found = i;
+            break;
+        }
+    }
+
+    if(found != -1) return index;
+    else return false;
+    
+}
+
+
+Blog.prototype.get = function(title){
+
+    var index = this.indexOf(title);
+
+    if(index) return this.articles[index];
+    else return false;
+    
+}
+
+
+Blog.prototype.remove = function(title){
+    var index = this.indexOf(title);
+
+    if(index) {
+        this.articles.splice(index, 1);
+        return true;
+    } else return false;
+    
+}
+
+
+function convert(text){
+    return converter.makeHtml(text);
+}
+
+
+
+blog = new Blog("https://ream.systems/blog/");
+
 var articles = [
     {
         title:'example',
+        description:'Example blog post!',
         date:'13/5/2020',
-        path:'example/example.md'
+        path:'example',
+        img:'images/car.jpg'
     }
 ]
 
-read(articles[0].path);
+articles.forEach(article =>{
+    blog.add(article);
+})
 
-function read(path){
-    fetch("https://ream.systems/blog/" + path)
-    .then(response => {
-        return response.text();
-    }).then(text =>{
-        console.log(text);
-    })
-}
-
-function convert(text){
-    console.log(converter.makeHtml(text));
-}
-
+console.log(blog.get('example').html);
