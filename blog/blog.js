f4478113671164bb534e15344e614b0d80ed13f1
OliverBrotchie <o.brotchie@gmail.com>
2020-05-14 11:35:13 +0100

    another small update

--- blog/blog.js
+++ blog/blog.js
@@ -20,7 +20,7 @@ fetch('https://ream.systems/blog/articles.json').then((response)=>{
 
     Blog.prototype.add = async function(article){
 
-        if(article.html == undefined && window.location.href.includes('articles')){
+        if(article.html == undefined && window.location.href.includes('article')){
 
             await fetch(this.path + "/" + article.path + "/" + article.title + ".md")
             .then(response => {
@@ -97,7 +97,7 @@ fetch('https://ream.systems/blog/articles.json').then((response)=>{
                 addArticles().then(()=>{
 
                     //individual article
-                    if(window.location.href.includes('articles')){
+                    if(window.location.href.includes('article')){
 
 
                         var url = window.location.href.split('#');
@@ -125,8 +125,6 @@ fetch('https://ream.systems/blog/articles.json').then((response)=>{
 
                         } else {
 
-                            
-
                             for(var i = 0;i<titles.length;i++){
                                 titles[i].innerHTML = 'error';
                             }
@@ -147,7 +145,7 @@ fetch('https://ream.systems/blog/articles.json').then((response)=>{
                             if(!window.location.href.includes('blog')){
     
                                 elem.classList.add('case');
-                                elem.innerHTML = '<div class="row"><div class="col-md-6 col-lg-6 col-xl-8 d-flex"><a href="https://ream.systems/articles#'+ blog.articles[i].title +'" class="img w-100 mb-3 mb-md-0" style="background-image: url(' + blog.path + '/' + blog.articles[i].path + '/' + blog.articles[i].img + ');"></a></div><div class="col-md-6 col-lg-6 col-xl-4 d-flex"><div class="text w-100 pl-md-3"><span class="subheading">' + blog.articles[i].description + '</span><h2><a href="https://ream.systems/articles#'+ blog.articles[i].title +'">'+ blog.articles[i].title + '</a></h2><div class="meta"><p class="mb-0"><a>'+ blog.articles[i].date +'</a></p></div></div></div></div>';    
+                                elem.innerHTML = '<div class="row"><div class="col-md-6 col-lg-6 col-xl-8 d-flex"><a href="https://ream.systems/article#'+ blog.articles[i].title +'" class="img w-100 mb-3 mb-md-0" style="background-image: url(' + blog.path + '/' + blog.articles[i].path + '/' + blog.articles[i].img + ');"></a></div><div class="col-md-6 col-lg-6 col-xl-4 d-flex"><div class="text w-100 pl-md-3"><span class="subheading">' + blog.articles[i].description + '</span><h2><a href="https://ream.systems/article#'+ blog.articles[i].title +'">'+ blog.articles[i].title + '</a></h2><div class="meta"><p class="mb-0"><a>'+ blog.articles[i].date +'</a></p></div></div></div></div>';    
     
                             //blog
                             } else {
@@ -155,7 +153,7 @@ fetch('https://ream.systems/blog/articles.json').then((response)=>{
                                 elem.classList.add('col-md-4');
                                 elem.classList.add('d-flex');
                                 //elem.classList.add('ftco-animate');
-                                elem.innerHTML = '<div class="blog-entry justify-content-end"><a href="https://ream.systems/articles#'+ blog.articles[i].title + '" class="block-20" style="background-image: url(' + blog.path + '/' + blog.articles[i].path + '/' + blog.articles[i].img + ');"></a><div class="text p-4 float-right d-block"><div class="topper d-flex align-items-center"><div class="one py-2 pl-3 pr-1 align-self-stretch"><span class="day">'+ blog.articles[i].date.split('-')[0] + '</span></div><div class="two pl-0 pr-3 py-2 align-self-stretch"><span class="yr">' + blog.articles[i].date.split('-')[2] + '</span> <span class="mos">' + blog.articles[i].date.split('-')[1] + '</span></div></div><h3 class="heading mb-3"><a href="https://ream.systems/articles#'+ blog.articles[i].title + '">'+ blog.articles[i].title +'</a></h3><p>'+ blog.articles[i].description+'</p><p><a href="https://ream.systems/articles#'+ blog.articles[i].title + '" class="btn-custom"><span class="ion-ios-arrow-round-forward mr-3"></span>Read more</a></p></div></div>';
+                                elem.innerHTML = '<div class="blog-entry justify-content-end"><a href="https://ream.systems/article#'+ blog.articles[i].title + '" class="block-20" style="background-image: url(' + blog.path + '/' + blog.articles[i].path + '/' + blog.articles[i].img + ');"></a><div class="text p-4 float-right d-block"><div class="topper d-flex align-items-center"><div class="one py-2 pl-3 pr-1 align-self-stretch"><span class="day">'+ blog.articles[i].date.split('-')[0] + '</span></div><div class="two pl-0 pr-3 py-2 align-self-stretch"><span class="yr">' + blog.articles[i].date.split('-')[2] + '</span> <span class="mos">' + blog.articles[i].date.split('-')[1] + '</span></div></div><h3 class="heading mb-3"><a href="https://ream.systems/article#'+ blog.articles[i].title + '">'+ blog.articles[i].title +'</a></h3><p>'+ blog.articles[i].description+'</p><p><a href="https://ream.systems/article#'+ blog.articles[i].title + '" class="btn-custom"><span class="ion-ios-arrow-round-forward mr-3"></span>Read more</a></p></div></div>';
     
                             }
                             
